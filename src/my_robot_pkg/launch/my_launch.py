1121161978c299e555ab3d2a2e6cef55cdb1c51c
wooyoung-4723 <161607800+wooyoung-4723@users.noreply.github.com>
2026-03-27 16:46:02 +0900

    my_launch.py

--- src/my_robot_pkg/launch/my_launch.py
+++ src/my_robot_pkg/launch/my_launch.py
@@ -5,21 +5,18 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
 
-        # turtlesim 실행
         Node(
             package='turtlesim',
             executable='turtlesim_node',
             name='turtlesim'
         ),
 
-        # lidar publisher 실행
         Node(
             package='my_robot_pkg',
             executable='lidar_publisher',
             name='lidar_publisher'
         ),
 
-        # rosbridge websocket 실행
         Node(
             package='rosbridge_server',
             executable='rosbridge_websocket',
